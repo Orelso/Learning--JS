48339985e5ea1bbe31c6200a9b121828f0ddb52a
Orelso Vitam <orelso.vitam@avanio.com>
2023-01-30 12:34:36 +0200

    object method

--- src/JS101/ObjectMethods.js
+++ src/JS101/ObjectMethods.js
@@ -1,21 +1,34 @@
-const gohan = {
-    firstName: 'Gohan',
-    lastName: 'Son',
-    birthYear: 1991,
-    job: "Fighter",
-    friends: ['Piccolo', "Krillin", "Tien"],
-    canGoSuperSayian: true,
+const person1 = {
+    firstName: "Mark",
+    lastName: "Miller",
+    mass: 78,
+    height: 1.69,
 
-    //* passed a function as a property of gohan
-    calcAge: function(birthYear) {
+    calcBMI: function(mass, height) {
         console.log(this)
-        this.age = 2037 - this.birthYear
-        return this.age
+        this.BMI = this.mass / (this.height * this.height)
+        return this.BMI
     }
 }
-//* Will console the same thing. Different ways of calling it.
-console.log(gohan.age);
-console.log(gohan.calcAge(gohan.birthYear));
 
-//* ? if : else
-console.log(`${gohan.firstName} is a ${gohan.age}-year old ${gohan.job}, and he ${gohan.canGoSuperSayian ? 'can' : "can't"} go Super Saiyan.`)
+console.log(person1.calcBMI(person1.height))
+
+const person2 = {
+    firstName: "John",
+    lastName: "Smith",
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function(mass, height) {
+        console.log(this)
+        this.BMI = this.mass / (this.height * this.height)
+        return this.BMI
+    }
+}
+
+console.log(person2.calcBMI(person1.height))
+if(person1.BMI > person2.BMI){
+    console.log(`Hello ${person1.firstName} ${person1.lastName} your BMI ${person1.BMI}is much higher than ${person2.firstName} ${person2.lastName} ${person2.BMI}`)
+} else {
+    console.log(`Hello ${person2.firstName} ${person2.lastName} your BMI ${person2.BMI} is much higher than ${person1.firstName} ${person1.lastName} ${person1.BMI}`);
+}
